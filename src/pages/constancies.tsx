0576053ee71c65f5f858c3ed6d99db8f0b73ca00
Slisandro <salvareschilisandro0@gmail.com>
2025-02-07 17:14:11 -0300

    constancies page

---
+++ src/pages/constancies.tsx
@@ -0,0 +1,29 @@
+import TableComponent from "../components/table-component";
+import { useAllConstancies } from "../hooks/use-all-constancies-hook"
+
+export const ConstanciesPage = () => {
+    const allConstancies = useAllConstancies();
+
+    if(allConstancies.isLoading) return <>Loading</>
+
+    if(allConstancies.error) {
+        alert(allConstancies.error);
+    }
+
+    if(!allConstancies.data?.getAllConstancies) return <>Not found</>
+
+    return (
+        <div className="w-full row-start-2 h-full p-6 flex items-start gap-0 justify-start">
+            <TableComponent 
+                columns={[
+                    "id",
+                    "name",
+                    "email",
+                    "role"
+                ]}
+                data={allConstancies.data.getAllConstancies}
+                countPerPage={10}
+            />
+        </div>
+    )
+}
