598d942e4c327942c9fa0c632aa9bfec93d8c182
Slisandro <salvareschilisandro0@gmail.com>
2025-02-07 17:14:00 -0300

    courses page

---
+++ src/pages/courses.tsx
@@ -0,0 +1,28 @@
+import TableComponent from "../components/table-component";
+import { useAllCourses } from "../hooks/use-all-courses-hook";
+
+export const CoursesPage = () => {
+    const allCourses = useAllCourses();
+
+    if (allCourses.isLoading) return <>Loading</>
+
+    if (allCourses.error) {
+        alert(allCourses.error);
+    }
+
+    if (!allCourses.data?.getAllCourses) return <>Not found</>
+
+    return (
+        <div className="w-full row-start-2 h-full p-6 flex items-start gap-0 justify-start">
+            <TableComponent
+                columns={[
+                    "id",
+                    "name",
+                    "duration"
+                ]}
+                data={allCourses.data.getAllCourses}
+                countPerPage={10}
+            />
+        </div>
+    )
+}
