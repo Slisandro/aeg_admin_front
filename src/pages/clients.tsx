9653853de7e548cacbfc65452a30546ce280aa70
Slisandro <salvareschilisandro0@gmail.com>
2025-02-07 17:14:20 -0300

    clients page

---
+++ src/pages/clients.tsx
@@ -0,0 +1,29 @@
+import TableComponent from "../components/table-component";
+import { useAllClients } from "../hooks/use-all-clients-hook";
+
+export const ClientsPage = () => {
+    const allClients = useAllClients();
+
+    if (allClients.isLoading) return <>Loading</>
+
+    if (allClients.error) {
+        alert(allClients.error);
+    }
+
+    if (!allClients.data?.getAllClients) return <>Not found</>
+
+    return (
+        <div className="w-full row-start-2 h-full p-6 flex items-start gap-0 justify-start">
+            <TableComponent
+                columns={[
+                    "id",
+                    "name",
+                    "representative",
+                    "code"
+                ]}
+                data={allClients.data.getAllClients}
+                countPerPage={10}
+            />
+        </div>
+    )
+}
