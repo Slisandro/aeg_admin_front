fabc81c6488fc461d8574f0a6c13f3745c47ce8c
Slisandro <salvareschilisandro0@gmail.com>
2025-02-07 17:19:20 -0300

    all constancies hook

---
+++ src/hooks/use-all-constancies-hook.tsx
@@ -0,0 +1,38 @@
+import { useQuery } from 'react-query';
+
+const fetchAllConstancies = async () => {
+    const response = await fetch('https://4qpek8qkm8.execute-api.us-east-2.amazonaws.com/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            query: `
+                query {
+                    getAllConstancies {
+                        id
+                    }
+                }
+            `,
+        }),
+    });
+
+    if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData?.errors?.[0]?.message || 'Error al obtener cantidad de usuarios');
+    }
+
+    const { data }: { data: { getAllConstancies: any[] } } = await response.json();
+    return data;
+};
+
+export const useAllConstancies = () => {
+    return useQuery('allConstancies', fetchAllConstancies, {
+        staleTime: Infinity, // Datos no se consideran "stale" inmediatamente
+        cacheTime: Infinity, // Datos se mantienen en caché indefinidamente
+        // Otras opciones útiles:
+        // refetchOnMount: false,
+        // refetchOnWindowFocus: false,
+        // retry: 3,
+    });
+};
