3966419ef64583246fcdb25722306f7579d0f679
Slisandro <salvareschilisandro0@gmail.com>
2025-02-11 11:08:12 -0300

    add i18n context and translation

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -3,9 +3,12 @@ import CardStatisticsComponent from "../components/card-statistics-component"
 import { useUsersCount } from "../hooks/use-users-count-hook";
 import { useClientsCount } from '../hooks/use-client-count-hook';
 import { useCoursesCount } from '../hooks/use-courses-count-hook';
+import { LanguageContext } from '../context/i18n-context';
+import { useContext } from 'react';
+import i18n from '../i18n/config';
 
 export const DashboardPage = () => {
-
+    const {} = useContext(LanguageContext);
     const usersCount = useUsersCount();
     const clientsCount = useClientsCount();
     const coursesCount = useCoursesCount();
@@ -18,10 +21,10 @@ export const DashboardPage = () => {
 
     return (
         <div className="w-full row-start-2 h-full p-6 flex items-start gap-0 justify-start">
-            <CardStatisticsComponent title={String(usersCount.data?.usersCount)} subtitle='Users created' Icon={ClientsIcon} />
-            <CardStatisticsComponent title={String(clientsCount.data?.clientsCount)} subtitle='Clients created' Icon={ClientsIcon} />
-            <CardStatisticsComponent title={String(coursesCount.data?.coursesCount)} subtitle='Courses created' Icon={ClientsIcon} />
-            <CardStatisticsComponent title={String(coursesCount.data?.coursesCount)} subtitle='Constancies created' Icon={ClientsIcon} />
+            <CardStatisticsComponent title={String(usersCount.data?.usersCount)} subtitle={i18n.t("modules.users.statistics.title")} Icon={ClientsIcon} />
+            <CardStatisticsComponent title={String(clientsCount.data?.clientsCount)} subtitle={i18n.t("modules.clients.statistics.title")} Icon={ClientsIcon} />
+            <CardStatisticsComponent title={String(coursesCount.data?.coursesCount)} subtitle={i18n.t("modules.courses.statistics.title")} Icon={ClientsIcon} />
+            <CardStatisticsComponent title={String(coursesCount.data?.coursesCount)} subtitle={i18n.t("modules.constancies.statistics.title")} Icon={ClientsIcon} />
             <div></div>
         </div>
     )
