47e61324e6c98f61e938eee5f9a269b0a891a5f4
Slisandro <salvareschilisandro0@gmail.com>
2025-02-11 11:05:56 -0300

    change name in items-sidebar

--- src/constants/ITEMS_SIDEBAR.ts
+++ src/constants/ITEMS_SIDEBAR.ts
@@ -1,34 +1,34 @@
 // import for each svg icon 
-import { ReactComponent as HomeIcon } from '../assets/icons/home-icon.svg';
-import { ReactComponent as CoursesIcon } from '../assets/icons/courses-icon.svg';
-import { ReactComponent as UsersIcon } from '../assets/icons/users-icon.svg';
-import { ReactComponent as ConstanciesIcon } from '../assets/icons/constancies-icon.svg';
 import { ReactComponent as ClientsIcon } from '../assets/icons/clients-icon.svg';
+import { ReactComponent as ConstanciesIcon } from '../assets/icons/constancies-icon.svg';
+import { ReactComponent as CoursesIcon } from '../assets/icons/courses-icon.svg';
+import { ReactComponent as HomeIcon } from '../assets/icons/home-icon.svg';
+import { ReactComponent as UsersIcon } from '../assets/icons/users-icon.svg';
 import ItemSideBar from "../interfaces/item-sidebar-interface";
 
 export const ITEMS_SIDEBAR : ItemSideBar[] = [
     {
-        name: "Dashboard",
+        name: "sidebar.home",
         Icon: HomeIcon,
         route: "/"
     },
     {
-        name: "Courses",
+        name: "sidebar.courses",
         Icon: CoursesIcon,
         route: "/courses"
     },
     {
-        name: "Users",
+        name: "sidebar.users",
         Icon: UsersIcon,
         route: "/users"
     },
     {
-        name: "Clients",
+        name: "sidebar.clients",
         Icon: ClientsIcon,
         route: "/clients"
     },
     {
-        name: "Constancies",
+        name: "sidebar.constancies",
         Icon: ConstanciesIcon,
         route: "/constancies"
     },
