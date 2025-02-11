206f9b0dbb069ff8bbfb70eb4437f3cf994434c0
Slisandro <salvareschilisandro0@gmail.com>
2025-02-11 11:05:09 -0300

    add i18n context and handleChangeLanguage in languages dropdown

--- src/components/languages-dropdown-component.tsx
+++ src/components/languages-dropdown-component.tsx
@@ -1,17 +1,26 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { LANGUAGES } from "../constants/LANGUAGES";
 import { Option } from "../interfaces/option-interface";
 import ButtonLanguageComponent from "./button-languages-component";
 import DropDownComponent from "./dropdown-component";
+import i18n from "../i18n/config";
+import { LanguageContext } from "../context/i18n-context";
 
-export default function DropDownLanguagesComponent () {
-    const [lang, setLang] = useState(1);
+export default function DropDownLanguagesComponent() {
+    const [lang, setLang] = useState(i18n.locale === "es" ? 2 : 1);
+    const { setLocale } = useContext(LanguageContext);
+
+    const handleChangeLanguage = ({ key }: Option) => {
+        setLang(key)
+        setLocale(key === 1 ? "en" : "es");
+        i18n.locale = key === 1 ? "en" : "es"
+    }
 
     return (
         <DropDownComponent
             options={LANGUAGES}
             children={<ButtonLanguageComponent lang={lang} />}
-            onSelect={(op: Option) => setLang(op.key)}
+            onSelect={handleChangeLanguage}
         />
     )
 }
