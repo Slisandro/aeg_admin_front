237503fcd44cb4ea6bc3ad51fb794a3a4b708c42
Slisandro <salvareschilisandro0@gmail.com>
2025-02-11 11:03:56 -0300

    add i18n context and translation in sidebar

--- src/components/side-bar-component.tsx
+++ src/components/side-bar-component.tsx
@@ -1,7 +1,12 @@
 import { NavLink } from "react-router-dom";
 import { ITEMS_SIDEBAR } from "../constants/ITEMS_SIDEBAR";
+import i18n from "../i18n/config";
+import { useContext } from "react";
+import { LanguageContext } from "../context/i18n-context";
 
 export default function SideBarComponent() {
+    const { } = useContext(LanguageContext);
+
     return (
         <section className="h-full row-start-2 shadow-2xl w-[200px] flex flex-col items-center justify-start gap-12 py-12 bg-[#f7f7f8]">
             {ITEMS_SIDEBAR.map(({ name, Icon, route }) => <SideBarItem route={route} name={name} Icon={Icon} />)}
@@ -20,7 +25,7 @@ const SideBarItem = (
     <NavLink to={route} className="flex items-center justify-center gap-4 w-full h-10">
         <Icon className="h-5 w-5 fill-transparent" />
         <p className="text-black text-xl w-1/2 font-medium">
-            {name}
+            {i18n.t(name)}
         </p>
     </NavLink>
 )
