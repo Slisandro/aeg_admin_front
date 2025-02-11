61f4915ee540f6688fa23d3e259d05ee7aaae17b
Slisandro <salvareschilisandro0@gmail.com>
2025-02-11 11:06:47 -0300

    add i18n context

--- src/App.tsx
+++ src/App.tsx
@@ -6,22 +6,25 @@ import { ClientsPage } from './pages/clients';
 import { CoursesPage } from './pages/courses';
 import { ConstanciesPage } from './pages/constancies';
 import { UsersPage } from './pages/users';
+import { LanguageProvider } from './context/i18n-context';
 
 function App() {
   return (
-    <BrowserRouter>
-      <main className="w-screen h-screen bg-white-500 grid grid-cols-[max-content_1fr] grid-rows-[max-content_1fr] gap-0">
-        <HeaderComponent />
-        <SideBarComponent />
-        <Routes>
-          <Route path="/" element={<DashboardPage />} />
-          <Route path="/clients" element={<ClientsPage />} />
-          <Route path="/courses" element={<CoursesPage />} />
-          <Route path="/constancies" element={<ConstanciesPage />} />
-          <Route path="/users" element={<UsersPage />} />
-        </Routes>
-      </main>
-    </BrowserRouter>
+    <LanguageProvider>
+      <BrowserRouter>
+        <main className="w-screen h-screen bg-white-500 grid grid-cols-[max-content_1fr] grid-rows-[max-content_1fr] gap-0">
+          <HeaderComponent />
+          <SideBarComponent />
+          <Routes>
+            <Route path="/" element={<DashboardPage />} />
+            <Route path="/clients" element={<ClientsPage />} />
+            <Route path="/courses" element={<CoursesPage />} />
+            <Route path="/constancies" element={<ConstanciesPage />} />
+            <Route path="/users" element={<UsersPage />} />
+          </Routes>
+        </main>
+      </BrowserRouter>
+    </LanguageProvider>
   );
 }
 
