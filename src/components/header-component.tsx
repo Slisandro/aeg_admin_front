69ff65aad039b76c538acb92c51c7ca6c67655ef
Slisandro <salvareschilisandro0@gmail.com>
2025-02-12 18:44:58 -0300

    responsive design in header component and burguer menu

--- src/components/header-component.tsx
+++ src/components/header-component.tsx
@@ -1,10 +1,21 @@
+import { ITEMS_SIDEBAR_DROPDOWN } from "../constants/ITEMS_SIDEBAR_DROPDOWN";
+import DropDownComponent from "./dropdown-component";
 import DropDownLanguagesComponent from "./languages-dropdown-component";
 import UserLoggedComponent from "./user-logged-component";
 
 export default function HeaderComponent() {
     return (
         <header className="h-[70px] col-span-2 py-4 px-7 flex items-center justify-between bg-[#2D68A2] shadow-xl">
-            <h2 className="text-white text-2xl font-bold text-shadow-2xl text-shadow-red">AEG</h2>
+            <div className="lg:hidden flex gap-4 items-center">
+                <DropDownComponent
+                    children={<button className="p-2 text-xl font-bolder text-white">=</button>}
+                    onSelect={() => null}
+                    options={ITEMS_SIDEBAR_DROPDOWN}
+                    classNameDropdown="z-10 absolute top-14 left-0 bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-[16rem] dark:bg-gray-700 flex flex-col items-center list-none"
+                />
+                <h2 className="text-white text-2xl font-bold text-shadow-2xl text-shadow-red">AEG</h2>
+            </div>
+            <h2 className="hidden lg:block text-white text-2xl font-bold text-shadow-2xl text-shadow-red">AEG</h2>
             <div className="flex gap-4 items-center">
                 <form className="max-w-sm mx-auto">
                     <div className="flex relative">
