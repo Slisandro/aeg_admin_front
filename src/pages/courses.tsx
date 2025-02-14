bb671d76656f470a6e216196fcce2dca62e1a2e4
Slisandro <salvareschilisandro0@gmail.com>
2025-02-13 23:17:04 -0300

    fix course page

--- src/pages/courses.tsx
+++ src/pages/courses.tsx
@@ -1,24 +1,46 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
+import CourseFormComponent from "../components/form-courses-component";
+import ModalComponent from "../components/modal-component";
 import TableComponent from "../components/table-component";
 import { LanguageContext } from "../context/i18n-context";
-import { useAllCourses } from "../hooks/use-all-courses-hook";
 import i18n from "../i18n/config";
+import { useStore } from "../store";
 
 export const CoursesPage = () => {
-    const allCourses = useAllCourses();
     const { } = useContext(LanguageContext);
+    const { allCourses } = useStore();
 
-    if (allCourses.isLoading) return <>Loading</>
+    const [isModalCreateOpen, setModalCreateOpen] = useState(false);
+    const [isModalEditOpen, setModalEditOpen] = useState(false);
+    const [entity, setEntity] = useState();
 
-    if (allCourses.error) {
-        alert(allCourses.error);
-    }
+    useEffect(() => {
+        if (entity) {
+            setModalEditOpen(true);
+        }
+    }, [entity])
 
-    if (!allCourses.data?.getAllCourses) return <>Not found</>
+    const toggleModalCreate = () => setModalCreateOpen(!isModalCreateOpen);
+    const toggleModalEdit = () => {
+        setModalEditOpen(!isModalEditOpen);
+        setEntity(undefined)
+    };
+
+    if (!allCourses) return <>Not found</>
 
     return (
-        <div className="w-full col-span-2 lg:row-start-2 h-full p-6 flex items-start gap-0 justify-start">
+        <div className="w-full col-span-2 lg:row-start-2 h-full p-6 flex flex-col items-start gap-0 justify-start">
+            <div className="flex justify-end pb-4 px-2 w-full">
+                <ModalComponent
+                    isModalOpen={isModalCreateOpen}
+                    toggleModal={toggleModalCreate}
+                    children={<CourseFormComponent toggleModal={toggleModalCreate} />}
+                    title={i18n.t("modules.courses.action.create")}
+                    button={<button className="bg-[#2d68a2] font-bolder text-white rounded-md py-2 px-4  shadow-lg">{i18n.t("table.action.create+")}</button>}
+                />
+            </div>
             <TableComponent
+                setEntity={setEntity}
                 columnsKey={[
                     "id",
                     "name",
@@ -29,8 +51,15 @@ export const CoursesPage = () => {
                     i18n.t("modules.courses.table.columns.name"),
                     i18n.t("modules.courses.table.columns.duration"),
                 ]}
-                data={allCourses.data.getAllCourses}
-                countPerPage={allCourses.data.getAllCourses.length}
+                data={allCourses}
+                countPerPage={allCourses.length}
+            />
+            <ModalComponent
+                isModalOpen={isModalEditOpen}
+                toggleModal={toggleModalEdit}
+                children={<CourseFormComponent toggleModal={toggleModalEdit} entity={entity} />}
+                title={i18n.t("modules.courses.action.edit")}
+                button={<></>}
             />
         </div>
     )
