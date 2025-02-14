2d9deb01cd4a99dbc92d1505afc368c84e78a674
Slisandro <salvareschilisandro0@gmail.com>
2025-02-13 23:11:47 -0300

    add peticions in app.ts

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,45 @@ import { CoursesPage } from './pages/courses';
 import { ConstanciesPage } from './pages/constancies';
 import { UsersPage } from './pages/users';
 import { LanguageProvider } from './context/i18n-context';
+import { useAllClients } from './hooks/use-all-clients-hook';
+import { useEffect } from 'react';
+import { useStore } from './store';
+import { useAllConstancies } from './hooks/use-all-constancies-hook';
+import { useAllCourses } from './hooks/use-all-courses-hook';
+import { useAllUsers } from './hooks/use-all-users-hook';
 
 function App() {
+  const allClients = useAllClients();
+  const allConstancies = useAllConstancies();
+  const allCourses = useAllCourses();
+  const allUsers = useAllUsers();
+
+  const { getAllClients, getAllConstancies, getAllCourses, getAllUsers } = useStore();
+
+  useEffect(() => {
+    if (allClients.data?.getAllClients) {
+      getAllClients(allClients.data.getAllClients);
+    }
+
+    if (allConstancies.data?.getAllConstancies) {
+      getAllConstancies(allConstancies.data.getAllConstancies)
+    }
+
+    if (allCourses.data?.getAllCourses) {
+      getAllCourses(allCourses.data.getAllCourses)
+    }
+
+    if (allUsers.data?.getAllUsers) {
+      getAllUsers(allUsers.data.getAllUsers)
+    }
+  }, [
+    allClients.data?.getAllClients,
+    allConstancies.data?.getAllConstancies,
+    allCourses.data?.getAllCourses,
+    allUsers.data?.getAllUsers
+  ])
+
+
   return (
     <LanguageProvider>
       <BrowserRouter>
