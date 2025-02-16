9f1d87a6ea0a3f692ae45d5063c1f2bb91549ff3
Slisandro <salvareschilisandro0@gmail.com>
2025-02-16 16:40:29 -0300

    errors formik in form users

--- src/components/form-users-component.tsx
+++ src/components/form-users-component.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import useFormikHook from "../hooks/use-formik-hook";
 import i18n from '../i18n/config';
+import * as Yup from 'yup';
 
-interface ClientFormValues { // Otra interfaz diferente
+interface UserFormValues { // Otra interfaz diferente
     id: string,
     name: string,
     role: 'Admin' | 'User',
     email: string
 }
 
-const UserFormComponent = ({ toggleModal, entity }: { toggleModal: () => void, entity?: ClientFormValues }) => {
-    const initialValues: ClientFormValues = {
+const UsersFormSchema = Yup.object().shape({
+    name: Yup.string().required('Name is required'),
+    role: Yup.string().required('Role is required'),
+    email: Yup.string().email("Email invalid.").required('Email is required'),
+});
+
+const UserFormComponent = ({ toggleModal, entity }: { toggleModal: () => void, entity?: UserFormValues }) => {
+    const initialValues: UserFormValues = {
         id: entity?.id ?? '',
         name: entity?.name ?? '',
         role: entity?.role ?? 'User',
@@ -19,29 +26,33 @@ const UserFormComponent = ({ toggleModal, entity }: { toggleModal: () => void, e
 
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        console.log({ values });
-        toggleModal()
+        if(!Object.keys(errors).length) {
+            toggleModal()
+        }
     };
 
-    const { values, errors, handleChange } = useFormikHook(initialValues);
+    const { values, errors, handleChange, touched, handleBlur } = useFormikHook(initialValues, UsersFormSchema);
 
     return (
         <>
             <form className="flex flex-col gap-2 w-full" onSubmit={handleSubmit}>
                 <div className="my-2 flex flex-col w-full gap-2">
                     <label htmlFor="name" className="text-lg font-semibold">{i18n.t("modules.users.table.columns.name")}</label>
-                    <input type='text' name="name" value={values.name} onChange={handleChange} id="name" className="px-2 py-3 outline-none border-2 rounded-lg border-[rgba(0,0,0,.25)]" />
+                    <input type='text' name="name" value={values.name} onChange={handleChange} id="name" className={`px-2 py-3 outline-none border-2 rounded-lg ${touched.name && errors.name ? 'border-red-500' : 'border-[rgba(0,0,0,.25)]'}`} onBlur={handleBlur} />
+                    {touched.name && errors.name && (<p className="text-red-500 pl-2 font-bold">{errors.name}</p>)}
                 </div>
                 <div className="my-2 flex flex-col w-full w-full gap-2">
                     <label htmlFor="email" className="text-lg font-semibold">{i18n.t("modules.users.table.columns.email")}</label>
-                    <input className="px-2 py-3 outline-none border-2 rounded-lg border-[rgba(0,0,0,.25)]" type='email' name="email" value={values.email} onChange={handleChange} id="email" />
+                    <input className={`px-2 py-3 outline-none border-2 rounded-lg ${touched.email && errors.email ? 'border-red-500' : 'border-[rgba(0,0,0,.25)]'}`} onBlur={handleBlur} type='email' name="email" value={values.email} onChange={handleChange} id="email" />
+                    {touched.email && errors.email && (<p className="text-red-500 pl-2 font-bold">{errors.email}</p>)}
                 </div>
                 <div className="my-2 flex flex-col w-full gap-2">
                     <label htmlFor="role" className="text-lg font-semibold">{i18n.t("modules.users.table.columns.role")}</label>
-                    <select name="role" id="role" value={values.role} onChange={handleChange} className="px-2 py-3 outline-none border-2 rounded-lg border-[rgba(0,0,0,.25)]">
+                    <select name="role" id="role" value={values.role} onChange={handleChange} className={`px-2 py-3 outline-none border-2 rounded-lg ${touched.role && errors.role ? 'border-red-500' : 'border-[rgba(0,0,0,.25)]'}`} onBlur={handleBlur}>
                         <option value="Admin">Admin</option>
                         <option value="User">User</option>
                     </select>
+                    {touched.role && errors.role && (<p className="text-red-500 pl-2 font-bold">{errors.role}</p>)}
                 </div>
 
                 <div className="flex justify-end mt-6">
@@ -54,7 +65,6 @@ const UserFormComponent = ({ toggleModal, entity }: { toggleModal: () => void, e
                     </button>
                     <button
                         type="submit"
-                        disabled
                         className="bg-[#2D68A2] hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
                     >
                         {i18n.t("table.action.create")}
