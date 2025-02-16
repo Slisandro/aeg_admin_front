6dcdf202d9596c8c06a13848d16a726f99b9dbeb
Slisandro <salvareschilisandro0@gmail.com>
2025-02-16 16:41:32 -0300

    add validationSchema prop in formik hook

--- src/hooks/use-formik-hook.tsx
+++ src/hooks/use-formik-hook.tsx
@@ -1,10 +1,13 @@
 import { FormikValues, useFormik } from 'formik';
+import * as Yup from 'yup';
 
 export default function useFormikHook<T extends FormikValues>(
     initialValues: T,
+    validationSchema?: Yup.ObjectSchema<T>
 ) {
     const formik = useFormik<T>({
         initialValues,
+        validationSchema,
         onSubmit: () => {}
     });
 
