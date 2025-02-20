a1cd4f999fcecc5747ed6f6975b6a2ae3b92309f
Slisandro <salvareschilisandro0@gmail.com>
2025-02-19 21:27:52 -0300

    add input component

--- src/components/form-clients-component.tsx
+++ src/components/form-clients-component.tsx
@@ -1,29 +1,18 @@
 import React from 'react';
-import useFormikHook from "../hooks/use-formik-hook";
-import i18n from '../i18n/config';
-import * as Yup from 'yup';
 import { useCreateOrUpdateClient } from '../hooks/create-update-client-hook';
 import { useAllClients } from '../hooks/use-all-clients-hook';
+import useFormikHook from "../hooks/use-formik-hook";
+import i18n from '../i18n/config';
+import ClientFormValuesInterface from '../interfaces/client-form-values-interface';
+import ClientFormSchemaUtilities from '../utilities/yup-client-form-utilities';
+import InputComponent from './input-component';
 
-interface ClientFormValues { // Otra interfaz diferente
-    id: string,
-    name: string,
-    representative: string,
-    code: string
-};
-
-const ClientFormSchema = Yup.object().shape({
-    name: Yup.string().required('Name is required'),
-    representative: Yup.string().required('Representative is required'),
-    code: Yup.string().required('Code is required'),
-});
-
-const ClientFormComponent = ({ toggleModal, entity }: { toggleModal: () => void, entity?: ClientFormValues }) => {
+const ClientFormComponent = ({ toggleModal, entity }: { toggleModal: () => void, entity?: ClientFormValuesInterface }) => {
     const { mutate } = useCreateOrUpdateClient();
 
     const { refetch: refectAllClients } = useAllClients();
 
-    const initialValues: ClientFormValues = {
+    const initialValues: ClientFormValuesInterface = {
         id: entity?.id ?? '',
         name: entity?.name ?? '',
         representative: entity?.representative ?? '',
@@ -49,26 +38,41 @@ const ClientFormComponent = ({ toggleModal, entity }: { toggleModal: () => void,
         }
     };
 
-    const { values, errors, handleChange, touched, handleBlur } = useFormikHook(initialValues, ClientFormSchema);
+    const { values, errors, handleChange, touched, handleBlur } = useFormikHook(initialValues, ClientFormSchemaUtilities);
 
     return (
         <>
             <form className="flex flex-col gap-2 w-full" onSubmit={handleSubmit}>
-                <div className="my-2 flex flex-col w-full gap-2">
-                    <label htmlFor="name" className="text-lg font-semibold">{i18n.t("modules.clients.table.columns.name")}</label>
-                    <input type='text' name="name" value={values.name} onChange={handleChange} id="name" className={`px-2 py-3 outline-none border-2 rounded-lg ${touched.name && errors.name ? 'border-red-500' : 'border-[rgba(0,0,0,.25)]'}`} onBlur={handleBlur} />
-                    {touched.name && errors.name && (<p className="text-red-500 pl-2 font-bold">{errors.name}</p>)}
-                </div>
-                <div className="my-2 flex flex-col w-full gap-2">
-                    <label htmlFor="representative" className="text-lg font-semibold">{i18n.t("modules.clients.table.columns.representative")}</label>
-                    <input className={`px-2 py-3 outline-none border-2 rounded-lg ${touched.representative && errors.representative ? 'border-red-500' : 'border-[rgba(0,0,0,.25)]'}`} type='text' name="representative" value={values.representative} onChange={handleChange} id="representative" onBlur={handleBlur} />
-                    {touched.representative && errors.representative && (<p className="text-red-500 pl-2 font-bold">{errors.representative}</p>)}
-                </div>
-                <div className="my-2 flex flex-col w-full w-full gap-2">
-                    <label htmlFor="code" className="text-lg font-semibold">{i18n.t("modules.clients.table.columns.code")}</label>
-                    <input className={`px-2 py-3 outline-none border-2 rounded-lg ${touched.code && errors.code ? 'border-red-500' : 'border-[rgba(0,0,0,.25)]'}`} type='text' name="code" value={values.code} onChange={handleChange} id="code" onBlur={handleBlur} />
-                    {touched.code && errors.code && (<p className="text-red-500 pl-2 font-bold">{errors.code}</p>)}
-                </div>
+                <InputComponent  
+                    value={values.name}
+                    classNameContainer="my-2 flex flex-col w-full gap-2"
+                    handleBlur={handleBlur}
+                    handleChange={handleChange}
+                    error={errors.name}
+                    touched={touched.name} 
+                    name={"name"} 
+                    type={"string"}
+                />
+                <InputComponent  
+                    value={values.representative}
+                    classNameContainer="my-2 flex flex-col w-full gap-2"
+                    handleBlur={handleBlur}
+                    handleChange={handleChange}
+                    error={errors.representative}
+                    touched={touched.representative} 
+                    name={"representative"} 
+                    type={"string"}
+                />
+                <InputComponent  
+                    value={values.code}
+                    classNameContainer="my-2 flex flex-col w-full gap-2"
+                    handleBlur={handleBlur}
+                    handleChange={handleChange}
+                    error={errors.code}
+                    touched={touched.code} 
+                    name={"code"} 
+                    type={"string"}
+                />
 
                 <div className="flex justify-end mt-6">
                     <button
